0a5c6e499ed64e6eef02de60ee2f1148ec367647
baranadm <baranadm@gmail.com>
2022-04-06 14:35:37 +0200

    modified FigureMoves

---
+++ ChessGameSketch/Figures/FigureMove.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameSketch
+{
+    public class FigureMove
+    {
+        public Vector2 Direction { get; }
+        private bool Repeatable { get; }
+
+        public FigureMove(Vector2 direction, bool repeatable)
+        {
+            this.Direction = direction;
+            this.Repeatable = repeatable;
+        }
+
+    }
+
+}
