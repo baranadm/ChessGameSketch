d4920cc6369d76b55f584e2b2d6ef14926f23f18
baranadm <baranadm@gmail.com>
2022-04-06 15:08:35 +0200

    updated GameManager

--- ChessGameSketch/Board/GameManager.cs
+++ ChessGameSketch/Board/GameManager.cs
@@ -8,29 +8,27 @@ namespace ChessGameSketch
         public List<Vector2> GetAllowedMoves(Board board, Figure figureInspected)
         {
             List<Vector2> possibleMoves = new List<Vector2>();
-            possibleMoves.AddRange(GetAttackedFields(figureInspected, board));
-            
+            possibleMoves.AddRange(GetAccessiblePositions(figureInspected, board));
+
             List<Vector2> movesCausingCheck = new List<Vector2>();
 
-            // if current player has king on the board, simulate every move and dismiss every move, that causes check on king
-            // check, if there are player's kings
             List<Figure> friendlyKings = board.FindAll(FigureType.King, figureInspected.Player);
             if (friendlyKings.Any())
             {
                 Player opponent = figureInspected.Player.Equals(Player.White) ? Player.Black : Player.White;
 
-                foreach(Vector2 possibleMove in possibleMoves)
+                foreach (Vector2 possibleMove in possibleMoves)
                 {
                     Board simulatedBoard = board.GetCopy();
                     List<Figure> simulatedOpponentsFigures = simulatedBoard.FindAll(opponent);
                     Figure? simulatedFigureInspected = simulatedBoard.FigureOn(figureInspected.Position);
 
                     simulatedBoard.MoveFigure(simulatedFigureInspected, possibleMove);
-                    List<Figure> simulatedFriendlyKings = simulatedBoard.FindAll(FigureType.King,figureInspected.Player);
+                    List<Figure> simulatedFriendlyKings = simulatedBoard.FindAll(FigureType.King, figureInspected.Player);
                     List<Vector2> simulatedFriendlyKingsPositions = simulatedFriendlyKings.Select(king => king.Position).ToList();
 
-                    bool isAnyKingChecked = simulatedOpponentsFigures.Exists(fig => GetAttackedFields(fig, simulatedBoard).Exists(field => simulatedFriendlyKingsPositions.Contains(field)));
-                    if(isAnyKingChecked) movesCausingCheck.Add(possibleMove);
+                    bool isAnyKingChecked = simulatedOpponentsFigures.Exists(fig => GetAccessiblePositions(fig, simulatedBoard).Exists(field => simulatedFriendlyKingsPositions.Contains(field)));
+                    if (isAnyKingChecked) movesCausingCheck.Add(possibleMove);
                 }
             }
 
@@ -40,33 +38,33 @@ namespace ChessGameSketch
 
 
 
-        private List<Vector2> GetAttackedFields(Figure figureInspected, Board board)
+        private List<Vector2> GetAccessiblePositions(Figure figureInspected, Board board)
         {
-            FigureMoves figureMoves = figureInspected.GetFigureMoves();
-            List<Vector2> figureMovesDirections = figureMoves.Directions;
+            List<FigureMove> figureMoves = figureInspected.GetFigureMoves();
 
-            Pawn? pawn = figureInspected.GetFigureType().Equals(FigureType.Pawn) ? (Pawn) figureInspected : null;
+            Pawn? pawn = figureInspected.GetFigureType().Equals(FigureType.Pawn) ? (Pawn)figureInspected : null;
             if (pawn != null)
             {
-                foreach(Vector2 attackDirection in pawn.GetAttackDirections().Directions)
+                foreach (FigureMove attackMove in pawn.GetAttackMoves())
                 {
-                    Vector2 possibleAttackField = Vector2.Add(pawn.Position, attackDirection);
-                    Figure? possibleFigureAttacked = board.FigureOn(possibleAttackField);
-                    if (!possibleFigureAttacked.SamePlayerAs(pawn))
+                    Vector2 positionUnderAttack = Vector2.Add(pawn.Position, attackMove.Direction);
+                    Figure? figureUnderAttack = board.FigureOn(positionUnderAttack);
+                    if (figureUnderAttack != null && !figureUnderAttack.SamePlayerAs(pawn))
                     {
-                        figureMovesDirections.Add(attackDirection);
+                        figureMoves.Add(attackMove);
                     }
-                }                
+                }
             }
-            List<Vector2> attackedFields = new List<Vector2>();
-            for (int i = 0; i < figureMovesDirections.Count; i++)
+
+            List<Vector2> accessiblePositions = new List<Vector2>();
+            for (int i = 0; i < figureMoves.Count; i++)
             {
-                Vector2 actualDirection = figureMovesDirections[i];
+                FigureMove actualMove = figureMoves[i];
                 Vector2 actualPosition = figureInspected.Position;
 
                 while (true)
                 {
-                    Vector2 nextPosition = Vector2.Add(actualPosition, actualDirection);
+                    Vector2 nextPosition = Vector2.Add(actualPosition, actualMove.Direction);
 
                     bool outOfBounds = nextPosition.X < 0 || nextPosition.X > 7 || nextPosition.Y < 0 || nextPosition.Y > 7;
                     if (outOfBounds) break;
@@ -79,23 +77,23 @@ namespace ChessGameSketch
                         if (samePlayer) break;
                         else
                         {
-                            attackedFields.Add(nextPosition);
+                            accessiblePositions.Add(nextPosition);
                             break;
                         }
-                    } else
+                    }
+                    else
                     {
-                        attackedFields.Add(nextPosition);
+                        accessiblePositions.Add(nextPosition);
                         actualPosition.X = nextPosition.X;
                         actualPosition.Y = nextPosition.Y;
-                        //board.MoveFigure(figureInspected, nextPosition);
                     }
 
                     // break if move is not repetable
-                    if (!figureMoves.Repeatable) break;
+                    if (!actualMove.Repeatable) break;
                 }
             }
 
-            return attackedFields;
+            return accessiblePositions;
         }
 
     }
