bd2e9740bccbf4a49c02ab604b48a94b4a1ccfa3
baranadm <baranadm@gmail.com>
2022-04-13 11:04:59 +0200

    tiles[][] indexes matches tiles x,y properties

--- ChessGameFront/ChessGameFront/ChessGameFront/src/app/app.component.ts
+++ ChessGameFront/ChessGameFront/ChessGameFront/src/app/app.component.ts
@@ -13,28 +13,47 @@ export class AppComponent implements OnInit {
   private readonly httpClient;
   public figuresOnBoard: FigureOnBoard[] = [];
   public tiles: Tile[][] = [];
+  private activeFigure?: FigureOnBoard;
 
   constructor(http: HttpClient) {
     this.httpClient = http;
 
-    this.httpClient.get<FigureOnBoard[]>(this.API_URL).subscribe(result => this.loadFigures(result), error => console.error(error));
+    this.getFiguresFromApi().subscribe(result => this.loadFigures(result), error => console.error(error));
   }
+
+    private getFiguresFromApi() {
+        return this.httpClient.get<FigureOnBoard[]>(this.API_URL);
+    }
 
   ngOnInit(): void {
     this.tiles = this.generateBoard();
   }
 
-  onPieceClicked(clicked: any) {
+  onFigureClicked(clicked: any) {
     this.unmarkAllowedToMove();
+    this.activeFigure = clicked as FigureOnBoard;
     let figure = clicked as FigureOnBoard;
 
     this.tiles.forEach(row => row.forEach(tile => {
       if (tile.x == figure.x && tile.y == figure.y) tile.class += ' to-move-tile';
     }));
-    this.httpClient.get<AllowedToMove[]>(this.API_URL + '/getAllowedMoves/' + figure.id).subscribe(result => this.markAsAllowedToMove(result));
+    this.httpClient.get<Position[]>(this.API_URL + '/getAllowedMoves/' + figure.id).subscribe(result => this.markAsAllowedToMove(result));
   }
 
-  markAsAllowedToMove(result: AllowedToMove[]): void {
+  onTileClicked(destination: Tile) {
+    if (this.activeFigure != undefined) {
+      let actualFigure = this.activeFigure;
+      let desiredPosition: Position = {
+        x: destination.x,
+        y: destination.y
+      }
+      this.httpClient.post<FigureOnBoard[]>(this.API_URL + actualFigure.id, desiredPosition)
+        .subscribe(result => this.loadFigures(result),
+                    error => console.error(error));
+    }
+  }
+
+  markAsAllowedToMove(result: Position[]): void {
     this.tiles.forEach(row => row.forEach(tile => {
       let isAllowedToMove = result.find(move => move.x == tile.x && move.y == tile.y);
       if (isAllowedToMove) {
@@ -52,22 +71,27 @@ export class AppComponent implements OnInit {
   generateBoard(): Tile[][] {
     let tiles: Tile[][] = [];
     let asciiNumberOfLetterA = 65;
-    for (let y = 0; y <= 7; y++) {
-      let readableRowNumber: string = (-(y - 8)).toString();
+    for (let x = 0; x <= 7; x++) {
+      let readableRowNumber: string = (-(x - 8)).toString();
       let column: Tile[] = [];
-      for (let x = 7; x >= 0; x--) {
-        let readableColumnName = String.fromCharCode(asciiNumberOfLetterA + x);
-        let currentTileClass = (x + y) % 2 == 1 ? 'black-tile' : 'white-tile';
-        let tile: Tile = { x: x, y: 7-y,class: currentTileClass, occupiedBy: undefined, readablePosition: readableColumnName + readableRowNumber };
-        column[x] = tile;
+      for (let y = 0; y <= 7; y++) {
+        let readableColumnName = String.fromCharCode(asciiNumberOfLetterA + y);
+        let currentTileClass = (x + y) % 2 == 0 ? 'black-tile' : 'white-tile';
+        let tile: Tile = { x: x, y: y, class: currentTileClass, occupiedBy: undefined, readablePosition: readableColumnName + readableRowNumber };
+        column[y] = tile;
       }
-      tiles[y] = column;
+      tiles[x] = column;
     }
+
+    // reversing Y order, in order to make border look naturally
     return tiles;
   }
 
   loadFigures(result: FigureOnBoard[]) {
-    console.info(result);
+    // removes all figures from the board
+    this.figuresOnBoard = [];
+
+    // populates figures array
     result.forEach(fig => {
       this.figuresOnBoard.push({
         id: fig.id,
@@ -78,16 +102,26 @@ export class AppComponent implements OnInit {
         imagePath: imagePathFor(fig)
       });
     })
-    this.updateBoard();
+
+    this.flushTiles();
   }
 
-  updateBoard() {
-    this.tiles.forEach(row => {
-      row.forEach(tile => {
-        tile.occupiedBy = this.figuresOnBoard?.find(fig => fig.x == tile.x && fig.y == tile.y);
-      })
-    })
+  flushTiles() {
+    //this.forEachTile(tile => tile.occupiedBy = this.figuresOnBoard?.find(fig => fig.x == tile.x && fig.y == tile.y));
+    this.figuresOnBoard.forEach(fig => this.tiles[fig.x][fig.y].occupiedBy = fig);
+    //this.tiles.forEach(row => {
+    //  row.forEach(tile => {
+    //    tile.occupiedBy = this.figuresOnBoard?.find(fig => fig.x == tile.x && fig.y == tile.y);
+    //  })
+    //})
   }
+
+  //forEachTile(action: Function) {
+  //  this.tiles.forEach(row => {
+  //    row.forEach(tile => {
+  //      action(tile);
+  //    })
+  //}
 
   title = 'ChessGameFront';
 }
@@ -96,7 +130,7 @@ function imagePathFor(fig: FigureOnBoard): string {
   return 'assets/pieces/' + fig.figureType.toLowerCase() + fig.player.charAt(0).toUpperCase() + '.png';
 }
 
-interface AllowedToMove {
+interface Position {
   x: number;
   y: number;
 }
